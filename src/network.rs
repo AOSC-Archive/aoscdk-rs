fed5db25c99a4b5834b085207e265d04397bf293
eatradish <sakiiily@aosc.io>
2021-12-24 22:04:18 +0800

    frontend/tui: add mirror list speedtest feature

--- src/network.rs
+++ src/network.rs
@@ -1,10 +1,16 @@
 use anyhow::{anyhow, Result};
-use reqwest;
+use reqwest::{self, Url};
 use serde::{Deserialize, Serialize};
-use std::env::consts::ARCH;
+use sha2::{Digest, Sha256};
+use std::{
+    env::consts::ARCH,
+    time::{Duration, Instant},
+};
 
 const MANIFEST_URL: &str = "https://releases.aosc.io/manifest/recipe.json";
 const IS_RETRO: bool = cfg!(feature = "is_retro");
+const SPEEDTEST_FILE_CHECKSUM: &str = "98900564fb4d9c7d3b63f44686c5b8a120af94a51fc6ca595e1406d5d8cc0416";
+const USER_AGENT: &str = "Mozilla/5.0 (X11; AOSC OS; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0";
 
 // mirror manifests
 #[derive(Deserialize, Clone, Debug, Serialize)]
@@ -111,15 +117,58 @@ fn get_arch_name() -> Option<&'static str> {
     }
 }
 
-//         .user_agent("Mozilla/5.0 (X11; AOSC OS; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0")
 pub fn download_file(url: &str) -> Result<reqwest::blocking::Response> {
     let client = reqwest::blocking::ClientBuilder::new()
-        .user_agent("Mozilla/5.0 (X11; AOSC OS; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0")
+        .user_agent(USER_AGENT)
         .build()?;
     let resp = client.get(url).send()?;
     let resp = resp.error_for_status()?;
 
     Ok(resp)
+}
+
+pub fn speedtest_mirrors(mirrors: Vec<Mirror>) -> Vec<Mirror> {
+    let mut speedtest_mirror = vec![];
+    for mirror in &mirrors {
+        let score;
+        match get_mirror_speed_score(&mirror.url) {
+            Ok(s) => score = s,
+            Err(_) => {
+                continue
+            },
+        }
+        let name = &mirror.loc_tr;
+        speedtest_mirror.push((name, score));
+    }
+    speedtest_mirror.sort_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap());
+    let mut new_mirrors = vec![];
+    for (name, _) in speedtest_mirror {
+        let index = mirrors.iter().position(|x| &x.loc_tr == name).unwrap();
+        new_mirrors.push(mirrors[index].to_owned());
+    }
+
+    new_mirrors
+}
+
+fn get_mirror_speed_score(mirror_url: &str) -> Result<f32> {
+    let download_url = Url::parse(mirror_url)?.join("../misc/u-boot-sunxi-with-spl.bin")?;
+    let client = reqwest::blocking::ClientBuilder::new()
+        .user_agent(USER_AGENT)
+        .timeout(Duration::from_secs(10))
+        .build()?;
+    let timer = Instant::now();
+    let mut file = client.get(download_url).send()?;
+    let mut hasher = Sha256::new();
+    std::io::copy(&mut file, &mut hasher)?;
+    let result_time = timer.elapsed().as_secs_f32();
+    if hex::encode(hasher.finalize()) == SPEEDTEST_FILE_CHECKSUM {
+        return Ok(result_time);
+    }
+
+    Err(anyhow!(
+        "Failed to download test data from {}, please check your network connection!",
+        mirror_url
+    ))
 }
 
 pub fn find_variant_candidates(recipes: Recipe) -> Result<Vec<VariantEntry>> {
@@ -154,4 +203,12 @@ pub fn find_variant_candidates(recipes: Recipe) -> Result<Vec<VariantEntry>> {
     results.sort_by(|a, b| a.name.cmp(&b.name));
 
     Ok(results)
+}
+
+#[test]
+fn test_speedtest_mirrors() {
+    let manifest = fetch_recipe().map_err(|e| e.to_string()).unwrap();
+    let mirrors = fetch_mirrors(&manifest);
+    let new_mirrors = speedtest_mirrors(mirrors);
+    dbg!(new_mirrors);
 }
