bb016d53b31ae3f7586aaff9f95c3593477f414d
eatradish <sakiiily@aosc.io>
2022-02-01 13:34:54 +0800

    install: add bye_chroot function comment

--- src/install.rs
+++ src/install.rs
@@ -414,6 +414,7 @@ pub fn umount_all(mount_path: &Path, root_fd: Dir) {
     umount_root_path(mount_path).ok();
 }
 
+/// Check if env in chroot, Escape this env
 fn bye_chroot(root_fd: Dir) -> Result<()> {
     let output = Command::new("systemd-detect-virt").arg("-c").arg("-r").output()?;
     if output.status.success() {
