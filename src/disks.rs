789d30334ab41dc7e1c551b457d494eb168cefd1
eatradish <sakiiily@aosc.io>
2022-05-09 13:28:24 +0800

    disks: fix get_partition_table_type on ppc64/el

--- src/disks.rs
+++ src/disks.rs
@@ -216,10 +216,10 @@ pub fn right_combine(device_path: Option<&PathBuf>) -> Result<()> {
 #[cfg(target_arch = "powerpc64")]
 pub fn right_combine(device_path: Option<&PathBuf>) -> Result<()> {
     use crate::network;
-    let is_gpt = partition_is_gpt(device_path)?;
+    let partition_table_t = get_partition_table_type(device_path)?;
     let arch_name = network::get_arch_name();
 
-    if arch_name == Some("ppc64el") && !is_gpt {
+    if arch_name == Some("ppc64el") && partition_table_t != "gpt" {
         return Err(anyhow!("Installer detected an unsupported partition map for your device. Please use a GPT partition map for your POWER/CHRP-based device."));
     }
 
