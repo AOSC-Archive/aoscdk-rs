3c1721d29f0fe6e562da0221822146953c6bbcea
eatradish <sakiiily@aosc.io>
2022-06-20 09:36:57 +0800

    frontend/cli: improve output if could not get user input tarball name

--- src/frontend/cli.rs
+++ src/frontend/cli.rs
@@ -156,7 +156,7 @@ fn get_variant(tarball: &str) -> Result<VariantEntry> {
     }
 
     Err(anyhow!(
-        "Installer could not find tarball for specified variant {}.",
+        "Installer could not find tarball for specified variant {}.\nMaybe you can use --list-tarball and choose one?",
         tarball
     ))
 }
