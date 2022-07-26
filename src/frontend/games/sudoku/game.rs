980fc115e28584c9d4367ebc0a6606b7adf593c9
eatradish <sakiiily@aosc.io>
2022-07-26 12:45:09 +0800

    frontend/games/sudoku: remove useless button; add undo key bind

--- src/frontend/games/sudoku/game.rs
+++ src/frontend/games/sudoku/game.rs
@@ -1,7 +1,7 @@
 use crate::frontend::games::{add_callback, sudoku::board::SudokuBoard};
 use cursive::{
     traits::*,
-    views::{Button, Dialog, DummyView, LinearLayout, TextView},
+    views::{Dialog, LinearLayout, TextView},
     Cursive,
 };
 
@@ -11,42 +11,16 @@ pub fn run(siv: &mut Cursive) {
     siv.add_global_callback('q', quit);
     siv.add_global_callback('m', help);
     siv.add_global_callback('z', redo);
+    siv.add_global_callback('u', undo);
 
     siv.set_fps(2);
 
     let board = SudokuBoard::new();
 
-    let buttons1 = LinearLayout::horizontal()
-        .child(Button::new("Restart", restart))
-        .child(Button::new("Hint", hint))
-        .child(Button::new("Undo", undo))
-        .child(Button::new("Redo", redo));
-
-    let buttons2 = LinearLayout::horizontal()
-        .child(DummyView)
-        .child(DummyView)
-        .child(DummyView)
-        .child(DummyView)
-        .child(DummyView)
-        .child(DummyView)
-        .child(DummyView)
-        .child(DummyView)
-        .child(DummyView)
-        .child(DummyView)
-        .child(DummyView)
-        .child(DummyView)
-        .child(DummyView)
-        .child(DummyView)
-        .child(DummyView)
-        .child(Button::new("Help", help))
-        .child(Button::new("Quit", quit));
-
     let view = Dialog::around(
         LinearLayout::vertical()
             .child(board.with_name("board"))
             .child(TextView::new("Press 'm' to see manual"))
-            .child(buttons1)
-            .child(buttons2),
     )
     .title("SUDOKU");
 
@@ -88,5 +62,5 @@ fn redo(s: &mut Cursive) {
 }
 
 fn help(s: &mut Cursive) {
-    s.add_layer(Dialog::info("Use arrow keys/TAB/Shift+TAB/mouse wheel/mouse click to navigate.\nEnter number 0-9 to fill in.\nClick <Hint> or press <h> to obtain a hint.\nPress <z> to redo\nPress <r> to restart game\nPress <q> to exit game\nGood luck."))
+    s.add_layer(Dialog::info("Use arrow keys/TAB/Shift+TAB/mouse wheel/mouse click to navigate.\nEnter number 0-9 to fill in.\nClick <Hint> or press <h> to obtain a hint.\nPress <z> to redo\nPress <u> to undo\nPress <r> to restart game\nPress <q> to exit game\nGood luck."))
 }
