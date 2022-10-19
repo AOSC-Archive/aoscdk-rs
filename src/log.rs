0f5f5bf67a735e7d7e25cc0be173f4fc75a0871b
eatradish <sakiiily@aosc.io>
2022-10-19 16:21:40 +0800

    log: fix cursive logger view

--- src/log.rs
+++ src/log.rs
@@ -9,7 +9,7 @@ pub fn setup_logger(is_cli: bool) -> Result<PathBuf> {
     let now = OffsetDateTime::now_utc();
     let path = Path::new(&format!("/var/log/dklog-{}.log", now)).to_path_buf();
 
-    fern::Dispatch::new()
+    let fern_log = fern::Dispatch::new()
         .format(move |out, message, record| {
             let now = OffsetDateTime::now_utc();
             out.finish(format_args!(
@@ -21,8 +21,15 @@ pub fn setup_logger(is_cli: bool) -> Result<PathBuf> {
             ))
         })
         .level(log::LevelFilter::Info)
-        .chain(fern::log_file(&path)?)
-        .apply()?;
+        .chain(fern::log_file(&path)?);
+
+    let fern_log = if !is_cli {
+        fern_log.chain(Box::new(cursive::logger::get_logger()) as Box<dyn log::Log>)
+    } else {
+        fern_log
+    };
+
+    fern_log.apply()?;
 
     info!(
         "Using AOSC Deplotkit {} mode",
