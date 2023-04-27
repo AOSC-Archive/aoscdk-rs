f0f333b98bd5053129107b6c115b12993b1ed7a9
Mingcong Bai <jeffbai@aosc.xyz>
2023-04-27 08:56:29 -0700

    style: use cargo clippy to lint code

--- src/install.rs
+++ src/install.rs
@@ -397,7 +397,7 @@ fn set_full_name(full_name: &str, username: &str, passwd: Vec<&str>) -> Result<S
             continue;
         }
 
-        let mut i_split = i.split(":");
+        let mut i_split = i.split(':');
         // 用户名
         let i_username = i_split
             .next()
@@ -477,7 +477,7 @@ pub fn passwd_set_fullname(full_name: &str, username: &str) -> Result<()> {
     let mut buf = String::new();
     f.read_to_string(&mut buf)?;
 
-    let passwd = buf.split("\n").collect::<Vec<_>>();
+    let passwd = buf.split('\n').collect::<Vec<_>>();
 
     let s = set_full_name(full_name, username, passwd)?;
     f.seek(SeekFrom::Start(0))?;
@@ -769,7 +769,7 @@ nm-openconnect:x:104:104:NetworkManager user for OpenConnect:/:/sbin/nologin
 saki:x:1000:1001::/home/saki:/bin/bash
 pulse:x:58:58:PulseAudio Daemon Owner:/var/run/pulse:/bin/false
 _apt:x:976:976::/var/lib/apt:/sbin/nologin
-"#.split("\n").collect::<Vec<_>>();
+"#.split('\n').collect::<Vec<_>>();
 
     assert_eq!(set_full_name("Mag Mell", "saki", passwd.clone()).unwrap(), "root:x:0:0:root:/root:/bin/bash\nbin:x:1:1:bin:/dev/null:/bin/false\nnobody:x:99:99:Unprivileged User:/dev/null:/bin/false\ndbus:x:18:18:D-Bus Message Daemon User:/var/run/dbus:/bin/false\nsystemd-journal-gateway:x:994:994:systemd Journal Gateway:/:/sbin/nologin\nsystemd-bus-proxy:x:993:993:systemd Bus Proxy:/:/sbin/nologin\nsystemd-network:x:992:992:systemd Network Management:/:/sbin/nologin\nsystemd-resolve:x:991:991:systemd Resolver:/:/sbin/nologin\nsystemd-timesync:x:990:990:systemd Time Synchronization:/:/sbin/nologin\nsystemd-journal-remote:x:989:989:systemd Journal Remote:/:/sbin/nologin\nsystemd-journal-upload:x:988:988:systemd Journal Upload:/:/sbin/nologin\nldap:x:439:439:LDAP daemon owner:/var/lib/openldap:/bin/bash\nhttp:x:207:207:HTTP daemon:/srv/http:/bin/true\nuuidd:x:209:209:UUIDD user:/dev/null:/bin/true\nlocate:x:191:191:Locate daemon owner:/var/lib/mlocate:/bin/bash\npolkitd:x:27:27:PolicyKit Daemon Owner:/etc/polkit-1:/bin/false\nrtkit:x:133:133:RealtimeKit User:/proc:/sbin/nologin\nnamed:x:40:40:BIND DNS Server:/var/named:/sbin/nologin\ntss:x:159:159:Account used by the trousers package to sandbox the tcsd daemon:/dev/null:/sbin/nologin\nunbound:x:986:986:unbound:/etc/unbound:/bin/false\nsystemd-coredump:x:985:985:systemd Core Dumper:/:/sbin/nologin\nsystemd-nobody:x:65534:65534:Unprivileged User (systemd):/dev/null:/bin/false\nsystemd-oom:x:980:980:systemd Userspace OOM Killer:/:/usr/bin/nologin\nmysql:x:89:89:MariaDB Daemon User:/var/lib/mysql:/bin/false\ndnsmasq:x:488:6:dnsmasq daemon owner:/:/bin/nologin\npostgres:x:90:90:Postgres Daemon Owner:/var/lib/postgres:/bin/bash\navahi:x:84:84:Avahi Daemon Owner:/run/avahi-daemon:/bin/false\nmongodb:x:300:6:MongoDB daemon owner:/var/lib/mongodb:/bin/bash\ncolord:x:124:124:Color Daemon Owner:/var/lib/colord:/bin/false\nfcron:x:33:33::/var/spool/fcron:/bin/bash\nflatpak:x:979:979:Flatpak system helper:/:/usr/bin/nologin\nsaned:x:978:978:SANE Daemon Owner:/:/usr/bin/nologin\nsddm:x:977:977:Simple Desktop Display Manager Daemon Owner:/var/lib/sddm:/usr/bin/nologin\nrpc:x:332:332:Rpcbind Daemon:/dev/null:/bin/false\nusbmux:x:140:140:usbmux user:/:/sbin/nologin\nnm-openconnect:x:104:104:NetworkManager user for OpenConnect:/:/sbin/nologin\nsaki:x:1000:1001:Mag Mell:/home/saki:/bin/bash\npulse:x:58:58:PulseAudio Daemon Owner:/var/run/pulse:/bin/false\n_apt:x:976:976::/var/lib/apt:/sbin/nologin\n");
     assert!(set_full_name("Mag Mell\n", "saki", passwd.clone()).is_err());
