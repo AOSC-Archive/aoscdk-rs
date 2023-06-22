4b0275db4895bcd88313efdc7f2e229f3363acca
eatradish <sakiiily@aosc.io>
2023-06-22 21:12:00 +0800

    feat: limit unsquashfs thread to 1 if total ram <= 2GiB

--- src/install.rs
+++ src/install.rs
@@ -131,7 +131,17 @@ fn extract_squashfs<P: AsRef<Path>>(
     path: P,
     counter: cursive::utils::Counter,
 ) -> Result<()> {
-    distinst_squashfs::extract(archive, path, move |count| {
+    let mut sys = System::new_all();
+    sys.refresh_memory();
+    let total_memory = sys.total_memory() / 1024 / 1024 / 1024;
+
+    let limit_thread = if total_memory <= 2 {
+        Some(1)
+    } else {
+        None
+    };
+
+    distinst_squashfs::extract(archive, path, limit_thread, move |count| {
         counter.set((file_size * count as f64 / 100.0) as usize);
     })?;
 
